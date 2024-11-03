a98e270b1700c74b363dd3887f5119fc8399ce5d
ALPHISTUBE <rsalphi@gmail.com>
2024-11-03 18:08:58 +0600

    core

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pdfkit
 
 # Path to the HTML file
-html_file_path = 'pdf_format/index.html'
+import os
+
+# Path to the HTML file
+html_file_path = os.path.abspath('pdf_format/index.html')
 
 # Convert HTML file to PDF
 pdfkit.from_file(html_file_path, 'output.pdf')
